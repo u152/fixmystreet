f53c4bd0f5437d346293d19f650fc6c8b357632e
Dave Arter <davea@mysociety.org>
2018-03-12 11:31:21 +0000

    Improve wording/grammar when report is sent to more than two bodies

--- perllib/FixMyStreet/DB/Result/Problem.pm
+++ perllib/FixMyStreet/DB/Result/Problem.pm
@@ -659,16 +659,21 @@ sub body {
         }
     } else {
         my $bodies = $problem->bodies;
-        $body = join( _(' and '),
-            map {
-                my $name = $_->name;
-                if ($c and FixMyStreet->config('AREA_LINKS_FROM_PROBLEMS')) {
-                    '<a href="' . $_->url($c) . '">' . $name . '</a>';
-                } else {
-                    $name;
-                }
-            } values %$bodies
-        );
+        my @body_names = sort map {
+            my $name = $_->name;
+            if ($c and FixMyStreet->config('AREA_LINKS_FROM_PROBLEMS')) {
+                '<a href="' . $_->url($c) . '">' . $name . '</a>';
+            } else {
+                $name;
+            }
+        } values %$bodies;
+        if ( scalar @body_names > 2 ) {
+            my $l = scalar @body_names;
+            $body = join( _(', ', ), @body_names[0..$l-2]);
+            $body = join( _(', and '), ($body, $body_names[$l-1]));
+        } else {
+            $body = join( _(' and '), @body_names);
+        }
     }
     return $body;
 }
